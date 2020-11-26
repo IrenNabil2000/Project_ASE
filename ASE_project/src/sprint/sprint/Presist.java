db3d7a769838ce1001200f929f2a656182246a22
Nourhan-Amgad <64379169+Nourhan-Amgad@users.noreply.github.com>
2020-11-26 15:03:25 +0200

    subject validations

--- ASE_project/src/sprint/sprint/Presist.java
+++ ASE_project/src/sprint/sprint/Presist.java
@@ -20,7 +20,17 @@ public class Presist implements Ipresist {
 		n2.t.setCont(n2);
 		list.add(n2.t);
 	}
-
+	public boolean subjectValidations(String subject)
+	{ 
+		 for(int i=0;i<list.size();i++)
+	      {
+	        if(subject.equalsIgnoreCase(list.get(i).getSub()))
+	            return false;
+	           
+	      }
+	      
+		return true;
+	}
 	public boolean languageValidations(String languageInput) {
 		if(languageInput.equalsIgnoreCase("English") || languageInput.equalsIgnoreCase("French")) {
 			return true;
@@ -35,21 +45,45 @@ public class Presist implements Ipresist {
 		else
 			return false;
 	}
-	public Notification create(String subj, String lanaguge, String channel) {
-		Notification n = new Notification();
-		n.t.setSub(subj);
+	public Notification create(String subj,String lanaguge,String channel) {
+		Notification n=new Notification();
+		boolean flag=true;
+		boolean flag2=true;
+		if(subjectValidations(subj)==false)
+			{
+			System.out.println("ERROR THIS TEMPLATE ALREADY EXISTS");
+			
+			}
+		else {
+		
 		if(languageValidations(lanaguge))
-			n.t.setAvll(lanaguge);
+			{  
+			  
+			   flag=true;
+			}
 		else
-			System.out.println("Please choose English or French only");
+			{System.out.println("Please choose English or French only");
+			  flag=false;
+			}
 		if(channelValidations(channel))
-			n.t.setAvlc(channel);
+			{
+			 flag2=true;
+			}
 		else
-			System.out.println("Please enter the channel");
+			{System.out.println("Please choose Email or SMS only");
+			  flag2=false;
+			}
+		if(flag==true&& flag2==true)
+		{
+		
+		n.t.setSub(subj);
+		 n.t.setAvll(lanaguge);
+		 n.t.setAvlc(channel);
 		n.t.setCont(n);
 		list.add(n.t);
+		}
+		}
 		return n;
-
 	}
 
 	public void read() {
@@ -65,9 +99,15 @@ public class Presist implements Ipresist {
 		String input = scanner.nextLine();
 		String in = scanner.nextLine();
 
-		if (input.equals("subject")) {
-			n.t.setSub(in);
+		if(input.equals("subject"))
+		{ 
+			if(subjectValidations(in)==false)
+			System.out.println("Error this subject already exist");
+				
+		else {
+			n.t.setSub(in);	
 			n.t.setCont(n);
+		}
 		}
 		else if (input.equals("language"))
 		{
